76b767c398fd6280e0fa6b137a211ef5fbb3dc4b
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2024-10-29 15:01:06 -0300

    Implementacao atributos, gets, sets e toString de Hospede

--- src/model/Hospede.java
+++ src/model/Hospede.java
@@ -1,5 +1,45 @@
 package model;
 
-public class Hospede {
+public class Hospede extends Pessoa {
+	
+	private String dataNascimento;
+	private String endereco;
+	private String contato;
 
+	public Hospede(String nome, String cpf, String dataNascimento, String endereco, String contato) {
+		super(nome, cpf);
+		this.dataNascimento = dataNascimento;
+		this.endereco = endereco;
+		this.contato = contato;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	@Override
+	public String toString() {
+		return "Hospede [nome=" + super.getNome() +", cpf=" + super.getCpf() + ", dataNascimento=" + dataNascimento + ", endereco=" + endereco + ", contato=" + contato + "]";
+	}
+	
 }
