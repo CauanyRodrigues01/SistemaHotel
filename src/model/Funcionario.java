46a96392952b111d37af7c6e7c4565555695bcca
brunaxmello <bruna.mello@maisunifacisa.com.br>
2024-10-29 17:53:06 -0300

    Realizado estrutura da Classe Funcionario

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,5 +1,59 @@
 package model;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
+	
+	private String cargo;
+	private Double salario;
+	private String turno;
+	private Integer horasTrabalhadas;
+	
+	
+	public Funcionario(String nome, String cpf, String cargo, Double salario, String turno, Integer horasTrabalhadas) {
+		super(nome, cpf);
+		this.cargo = cargo;
+		this.salario = salario;
+		this.turno = turno;
+		this.horasTrabalhadas = horasTrabalhadas;
+	}
+	
+	
+	
+	public String getCargo() {
+		return cargo;
+	}
+
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+
+	public Double getSalario() {
+		return salario;
+	}
+
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+
+	public String getTurno() {
+		return turno;
+	}
+
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Funcionario [cargo=" + cargo + ", salario=" + salario + ", turno=" + turno + "]";
+	}
+	
+	
+	
 
 }
