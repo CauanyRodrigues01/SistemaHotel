577e11045d99cee6775f017ca2bd43e6c657d702
Jeffersonthiagofn <jeffersonthiagofn@gmail.com>
2024-11-01 12:04:08 -0300

    Realizado a implementaçao dos metodos cadastrar e editar na classe gerenciamento funcionário

--- src/controller/GerenciamentoFuncionario.java
+++ src/controller/GerenciamentoFuncionario.java
@@ -1,17 +1,48 @@
 package controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import model.Funcionario;
+import model.Hospede;
 
 public class GerenciamentoFuncionario implements Gerenciamento {
 
 	// atributos aqui!
+	private List<Funcionario> funcionarios;
 
 	// construtor aqui!
-
+	public GerenciamentoFuncionario() {
+		this.funcionarios = new ArrayList<>();
+	}
 	// metodos especificos aqui!
 
 	@Override
 	public void adicionar() {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("---- Você escolheu a opção para cadastrar um novo funcionário ---");
+
+		System.out.println("Informe o nome do novo funcionário: ");
+		String nomeNovoFuncionario = sc.nextLine();
+
+		System.out.println("Informe o CPF: ");
+		String cpfNovoFuncionario = sc.nextLine();
+
+		System.out.println("Informe o cargo: ");
+		String cargoNovoFuncionario = sc.nextLine();
+
+		System.out.println("Informe o salário por hora: ");
+		double salarioPorHoraNovoFuncionario = sc.nextDouble();
+
+		System.out.println("Informe o turno de trabalho: ");
+		String turnoNovoFuncionario = sc.nextLine();
+
+		Funcionario funcionario = new Funcionario(nomeNovoFuncionario, cpfNovoFuncionario, 
+				cargoNovoFuncionario, salarioPorHoraNovoFuncionario ,turnoNovoFuncionario, null);
 
 	}
 
