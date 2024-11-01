3a070b9c7a18b8d15fa18b36d058cbe1e153d488
Jeffersonthiagofn <jeffersonthiagofn@gmail.com>
2024-10-31 22:25:36 -0300

    Criado os metodos especificos na classe funcionario

--- src/controller/GerenciamentoFuncionario.java
+++ src/controller/GerenciamentoFuncionario.java
@@ -1,37 +1,38 @@
 package controller;
 
+
 public class GerenciamentoFuncionario implements Gerenciamento {
-	//atributos aqui!
-	
-	//construtor aqui!
-	
-	//metodos especificos aqui!
+
+	// atributos aqui!
+
+	// construtor aqui!
+
+	// metodos especificos aqui!
 
 	@Override
 	public void adicionar() {
-		// aqui pode pedir informacoes ao usuário com prints
-		// pense em o que acontece quando vc adiciona um novo funcionario: além de usar o construtor dele, depois você tem que adionar ele na lista de funcionarios dessa classe aqui
-		
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
 	public void editar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void excluir() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public void listar() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	//gets e sets e toString aqui!
+
+	// gets e sets e toString aqui!
 
 }
--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,56 +1,58 @@
 package model;
 
 public class Funcionario extends Pessoa {
-	
+
 	private String cargo;
-	private Double salario;
+	private Double salarioPorHora;
 	private String turno;
 	private Integer horasTrabalhadas;
-	
-	
-	public Funcionario(String nome, String cpf, String cargo, Double salario, String turno, Integer horasTrabalhadas) {
+
+	public Funcionario(String nome, String cpf, String cargo, Double salarioPorHora, String turno,
+			Integer horasTrabalhadas) {
 		super(nome, cpf);
 		this.cargo = cargo;
-		this.salario = salario;
+		this.salarioPorHora = salarioPorHora;
 		this.turno = turno;
 		this.horasTrabalhadas = horasTrabalhadas;
 	}
-	
-	//colocar funções específicas aqui depois do construtor!
-	
+
+	// colocar funções específicas aqui depois do construtor!
+
+	public void registrarHoras(int horas) {
+		this.horasTrabalhadas += horas;
+	}
+
+	public double calcularSalario() {
+		return horasTrabalhadas * salarioPorHora;
+	}
+
 	public String getCargo() {
 		return cargo;
 	}
-
 
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
 
-
-	public Double getSalario() {
-		return salario;
+	public Double getsalarioPorHora() {
+		return salarioPorHora;
 	}
 
-
-	public void setSalario(Double salario) {
-		this.salario = salario;
+	public void setsalarioPorHora(Double salarioPorHora) {
+		this.salarioPorHora = salarioPorHora;
 	}
-
 
 	public String getTurno() {
 		return turno;
 	}
 
-
 	public void setTurno(String turno) {
 		this.turno = turno;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Funcionario [cargo=" + cargo + ", salario=" + salario + ", turno=" + turno + "]";
+		return "Funcionario [cargo=" + cargo + ", salario=" + salarioPorHora + ", turno=" + turno + "]";
 	}
-	
+
 }
